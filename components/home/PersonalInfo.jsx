2f511625f8fd20fe84f9eed83a9dd1c72965afaf
Abdullatif <abdullatifk135@gmail.com>
2023-07-28 01:19:33 +0300

    finishing the design section

---
+++ components/home/PersonalInfo.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Image from "next/image";
+import apecWhite from "../../public/assets/svg/apeWhite.svg";
+import rnPerson from "../../public/assets/png&jpg/rnPerson.jpg";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import Link from "next/link";
+import NextLink from "next/link";
+const PersonalInfo = () => {
+  return (
+    <div className="mainPage">
+      <Image src={apecWhite} width={135.28} height={116} />
+      <div className="personalInfo">
+        <Image src={rnPerson} width={134} height={134} className="personImg" />
+        <div className="personNameCarrer">
+          <h2>
+            Mr.MOHAMAD
+            <br />
+            MOUSSAWI
+          </h2>
+          <p>Project Manager</p>
+        </div>
+        <div className="peronalSocial">
+          <div className="social">
+            <Link href="#" passHref className="iconBorder">
+              <FaInstagram className="socialIcon" />
+            </Link>
+          </div>
+          <div className="social">
+            <Link href="#" passHref className="iconBorder">
+              <FaFacebookF className="socialIcon" />
+            </Link>
+          </div>
+          <div className="social">
+            <Link href="#" passHref className="iconBorder">
+              <FaTiktok className="socialIcon" />
+            </Link>
+          </div>
+          <div className="social">
+            <Link href="#" passHref className="iconBorder">
+              <FaLinkedinIn className="socialIcon" />
+            </Link>
+          </div>
+        </div>
+        <div className="personContact">
+          <h3>
+            <Link href="tel:+961943581098" className="link">
+              +961943581098
+            </Link>
+          </h3>
+          <h3>Tripoli, North Lebanon</h3>
+          <h3>
+            <Link href="mailto:example@gmail.com" className="link">
+              example@gmail.com
+            </Link>
+          </h3>
+        </div>
+      </div>
+      <div className="download  redIcon">
+        <h3>Download</h3>
+        <p>our company profile</p>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInfo;
